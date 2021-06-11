39d38fc73cd267f83840525ff142f2bd77521472
“Jamie <knutsenjamie@yahoo.com>
2021-06-11 16:44:08 -0700

    WIP: Attempt to implement versioning. Unsure of what exact using statments to use

--- AnimalShelter/Controllers/DefaultController.cs
+++ AnimalShelter/Controllers/DefaultController.cs
@@ -1,20 +1,34 @@
-[Route("api/v{version:apiVersion}/[controller]")]
-[ApiController]
-[ApiVersion("1.0")]
-public class DefaultController : ControllerBase
-    {
-        string[] authors = new string[]
-        { "Jamie Knutsen" };
-        [HttpGet]
-        public IEnumerable<string> Get()
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiVersioning.Web.Contracts;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class DefaultController : ControllerBase
         {
-            return authors;
+            string[] authors = new string[]
+            { "Jamie Knutsen" };
+            [HttpGet]
+            public IEnumerable<string> Get()
+            {
+                return authors;
+            }
+            
+            [HttpGet("{id}")]
+            [MapToApiVersion("1.0")]
+            public string Get(int id)
+            {
+            return authors[id];
+            }
         }
-        
-        [HttpGet("{id}")]
-        [MapToApiVersion("1.0")]
-        public string Get(int id)
-        {
-        return authors[id];
-        }
-    }
+}
